808f4a1c33c591d55e891d57d8367ae52feefee2
Sam Rappl <samrappl50@gmail.com>
2019-06-21 00:56:46 -0400

    Added test skeleton framework

---
+++ test/lib_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class LibTest : public testing::Test {
+public:
+    virtual void SetUp() {
+
+    }
+
+    virtual void TearDown() {
+        
+    }
+}
+
+TEST_F(LibTest, AddOne) {
+
+}
+
+TEST_F(LibTest, TakeItOut) {
+
+}
